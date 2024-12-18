787c52748bccf78a8d39830ec06aa8e3ea8dc7dc
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-12-18 15:09:19 +0545

    learned about the statement

---
+++ statement.js
@@ -0,0 +1,70 @@
+// const gender = prompt("Enter the male for m or female for f");
+// if (gender === "m") {
+//   alert("You are male");
+// } else {
+//   alert("You are female");
+// }
+
+// Write a js program to check the day of the week
+// const day = Number(prompt("Enter the number 1 to 7"));
+
+// switch (day) {
+//   case 1:
+//     alert("Sunday");
+//     break;
+//   case 2:
+//     alert("Monday");
+//     break;
+//   case 3:
+//     alert("Tuesday");
+//     break;
+//   case 4:
+//     alert("Wednesday");
+//     break;
+//   case 5:
+//     alert("Thursday");
+//     break;
+//   case 6:
+//     alert("Friday");
+//     break;
+//   case 7:
+//     alert("Saturday");
+//     break;
+//   default:
+//     alert("You have to choose between 1 and 7");
+// }
+
+// Write a js program to check the grade of the student in switch case
+
+//A : >=80
+//B : >=60 but less than <80
+
+// switch (true) {
+//   case grade >= 80:
+//     alert("Your grade is A");
+//     break;
+//   case grade >= 60 && grade < 80:
+//     alert("Your grade is B");
+//     break;
+//   default:
+//     alert("Your have failed, Better luck next time !!")
+// }
+
+// Conditional Operator (Ternary Operator)
+
+// if() {}else() ES5
+
+// ES6 ma chai Ternary Operator hunxa.
+
+// ES5
+// const gender = "m";
+// if (gender === "m") {
+//   console.log("You are male");
+// } else {
+//   console.log("You are female");
+// }
+
+//ES6 (Ternary Operator)
+// Syntax =  condition ? statement true : statement false
+const gender = "m";
+gender === "m" ? console.log("m") : console.log("f");
